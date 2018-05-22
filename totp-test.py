23cb980ae90d54fdb0b88ab2f2991a1d1b2d06ae
gpinero <32255017+gpinero007@users.noreply.github.com>
2018-05-22 18:56:53 +0200

    Update totp-test.py

--- totp-test.py
+++ totp-test.py
@@ -1,25 +1,25 @@
 import pyotp
 import pyqrcode
 
-print ('Ejemplo validacion TOTP Python\n\n')
-print ('Generando QR Code ...')
+print ("--- Testing TOTP with python libraries \n\n")
+print ('Generating OTPAuth QR Code in file otpauth_qr.png (scan it with your app)')
+
 random_n = pyotp.random_base32()
 totp = pyotp.TOTP(random_n)
-qr = totp.provisioning_uri("gpinero@gmail.com", issuer_name="MasterCiberseguridad")
+qr = totp.provisioning_uri("gpinero@gmail.com", issuer_name="TEST TOTP Python")
 url = pyqrcode.create(qr)
 big_code = pyqrcode.create(qr, error='L', version=27, mode='binary')
-big_code.png('codigo_qr.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
-print ('Escanea la imagen png generado y valida un codigo----- \n')
+big_code.png('otpauth_qr.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+print ('-- Test TOTP code -- \n')
 
 loop=True
 while loop:
-	print ('El codigo TOTP en este instante es: '+ totp.now())
-	totp_input = input("Introduce el codigo generado por la aplicacion: (0 - salir)")
+	print ('TOTP code now is: '+ totp.now())
+	totp_input = input("Input the app code (0 - to exit):")
 	if totp.verify(str(totp_input)):
-		print ('El codigo ES VALIDO en este instante '+str(totp_input))
+		print ('\n ## OK your code is valid !!!'+str(totp_input))
 	else:
-		print ('El codigo NO ES VALIDO '+str(totp_input))
+		print ('\n WRONG code, not is '+str(totp_input))
 	if totp_input==0:
-		print "Adios ..."
-		loop=False
-	
+		print "Bye..."
+		loop=False	
